3cc2ec95d43e01ae7095935761b86182102614f6
Leo-Drew <drew.rose5@icloud.com>
2020-12-08 10:17:18 +0800

    online-mall后台登录拦截器实现

---
+++ src/main/java/com/drew/config/MvcConfig.java
@@ -0,0 +1,13 @@
+package com.drew.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/login.html","/admin/login","/css/*","/js/**","/image/**");
+    }
+}
--- src/main/java/com/drew/controller/LoginController.java
+++ src/main/java/com/drew/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -26,11 +27,13 @@ public class LoginController {
     }
 
     @RequestMapping("/admin/login")
-    public String adminLogin(@RequestParam("adminID") String adminID,@RequestParam("adminPwd") String adminPwd,Model model){
+    public String adminLogin(@RequestParam("adminID") String adminID, @RequestParam("adminPwd") String adminPwd, Model model, HttpSession session){
         List<Admin> admins=adminService.findAllAdmin();
         for (Admin admin:admins) {
-            if(admin.getAdminID().equals(adminID)&&admin.getAdminPwd().equals(adminPwd))
+            if(admin.getAdminID().equals(adminID)&&admin.getAdminPwd().equals(adminPwd)){
+                session.setAttribute("loginAdmin",admin.getAdminName());
                 return "redirect:/index.html";
+            }
         }
         model.addAttribute("msg","您的管理员ID或者密码有误！");
         return "login";
